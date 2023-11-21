8ea97fb61e737dcead5b219a6d3a9aa7cf96504e
Alex Haslam <alexhaslam12@gmail.com>
2023-11-21 20:12:54 +0000

    Configure authentication

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,14 @@
 import json
+import os
 
 import requests
 import streamlit as st
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+
+resource = "f727bsqbr5pihm444pfa7p4zf40jzfah"
+region = "eu-west-2"
+host = f"{resource}.lambda-url.{region}.on.aws"
+url = f"https://{host}/"
 
 ROAST_STYLES = ["Light", "Medium-Light", "Medium", "Medium-Dark", "Dark"]
 
@@ -49,7 +56,13 @@ else:
 
 flavours = st.multiselect("Flavour profile", options=FLAVOURS, format_func=lambda s: s.capitalize())
 
-url = "https://f727bsqbr5pihm444pfa7p4zf40jzfah.lambda-url.eu-west-2.on.aws/"
+auth = AWSRequestsAuth(
+    aws_access_key=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    aws_host=host,
+    aws_region="eu-west-2",
+    aws_service="lambda",
+)
 
 coffee = {
     "roaster": roaster,
@@ -59,7 +72,7 @@ coffee = {
     "price_per_100g": price_per_100g,
     "flavours": flavours,
 }
-response = requests.post(url, json=coffee)
+response = requests.post(url, json=coffee, auth=auth)
 response.raise_for_status()
 prediction = response.json()
 st.metric("Predicted rating", value="{:.2f}%".format(prediction["rating"]))
--- test_predict.py
+++ test_predict.py
@@ -1,6 +1,12 @@
-import requests
+import os
 
-url = "https://f727bsqbr5pihm444pfa7p4zf40jzfah.lambda-url.eu-west-2.on.aws/"
+import requests
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+
+resource = "f727bsqbr5pihm444pfa7p4zf40jzfah"
+region = "eu-west-2"
+host = f"{resource}.lambda-url.{region}.on.aws"
+url = f"https://{host}/"
 
 coffee = {
     "roaster": "Square Mile Coffee Roasters",
@@ -10,6 +16,14 @@ coffee = {
     "price_per_100g": 6.82,
     "flavours": ["fruity", "caramelly"],
 }
-response = requests.post(url, json=coffee)
+
+auth = AWSRequestsAuth(
+    aws_access_key=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    aws_host=host,
+    aws_region=region,
+    aws_service="lambda",
+)
+response = requests.post(url, json=coffee, auth=auth)
 response.raise_for_status()
 print(response.json())
